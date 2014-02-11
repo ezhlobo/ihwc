669d7e392707b78c193921d3e607105cc13b4d4a
vasiliSmiley <vasili.smiley@gmail.com>
2014-02-11 21:42:06 +0300

    Added table group

--- application.rb
+++ application.rb
@@ -1,18 +1,49 @@
 require 'sinatra'
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'sqlite3'
+
+ActiveRecord::Base.establish_connection(
+   :adapter => 'sqlite3',
+   :database => 'db/sinatra.sqlite'
+)
+
+class Team < ActiveRecord::Base
+   belongs_to :group
+end
+
+class Group < ActiveRecord::Base
+end
 
 configure do
   set :public_folder, Proc.new { File.join(root, "static") }
   enable :sessions
 end
 
-get '/hi' do
-  "Hello World!"
+get '/' do
+   @teams = Team.all
+   @groups = Group.all
+   erb :index
 end
 
-get "/" do
-	erb :index
+get '/teams' do
+   @teams = Team.all
+   erb :teams
 end
 
-get "/mrbug" do
-	erb :adminspage
+post '/teams' do
+   @team = Team.new(params)
+   @team.save
+   redirect to('/teams')
 end
+
+delete '/teams' do
+   @team = Team.find_by(:name => params[:name])
+   @team.destroy
+   redirect to('/teams')
+end
+
+
+
+
+
