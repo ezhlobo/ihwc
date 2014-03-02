099e79a26288177182057131cbc350b756e58927
Evgeny Zhlobo <evgen.zhlobo@gmail.com>
2014-03-02 06:42:35 +0300

    Remove sqlite

--- Gemfile
+++ Gemfile
@@ -2,17 +2,9 @@
 source "https://rubygems.org"
 ruby "2.0.0"
 
-gem "heroku"
 gem 'thin'
 gem "sinatra"
 gem "sinatra-activerecord"
 gem "sinatra-basic-auth"
 gem "sinatra-partial"
-
-group :development, :test do
-  gem 'sqlite3'
-end
-
-group :production do
-  gem 'pg'
-end
+gem 'pg'
--- application.rb
+++ application.rb
@@ -2,37 +2,8 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/partial'
-require 'sqlite3'
 
 register Sinatra::Partial
-
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => 'db/sinatra.sqlite'
-)
-
-class Team < ActiveRecord::Base
-  belongs_to :group
-  has_one :stat
-  has_and_belongs_to_many :games
-end
-
-class Group < ActiveRecord::Base
-  has_many :teams
-end
-
-class Stat < ActiveRecord::Base
-  belongs_to :team
-end
-
-class Game < ActiveRecord::Base
-  has_and_belongs_to_many :teams
-  has_one :result
-end
-
-class Result < ActiveRecord::Base
-  belongs_to :game
-end
 
 configure do
   set :public_folder, Proc.new { File.join(root, "assets") }
@@ -42,36 +13,5 @@ configure do
 end
 
 get '/' do
-  @teams = Team.all
-  @groups = Group.all
-  @stats = Stat.all
-  @games = Game.all
-  @results = Result.all
   erb :index
 end
-
-get '/teams' do
-  @teams = Team.all
-  @groups = Group.all
-  @stats = Stat.all
-  @games = Game.all
-  @results = Result.all
-  erb :teams
-end
-
-post '/teams' do
-  @team = Team.new(params)
-  @team.save
-  redirect to('/teams')
-end
-
-delete '/teams' do
-  @team = Team.find_by(:name => params[:name])
-  @team.destroy
-  redirect to('/teams')
-end
-
-
-
-
-
