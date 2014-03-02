5e15d1fefd4be83764b124684ca203b8cbe493c8
Evgeny Zhlobo <evgen.zhlobo@gmail.com>
2014-03-02 08:45:25 +0300

    Prepare to deploy

--- application.rb
+++ application.rb
@@ -1,44 +1,21 @@
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/activerecord'
 require 'sinatra/partial'
-require 'sqlite3'
+require 'sinatra/activerecord'
+
+require './config/environments'
+require './models/game'
+require './models/team'
+require './models/group'
+require './models/stat'
+require './models/result'
 
 register Sinatra::Partial
-
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => 'db/sinatra.sqlite'
-)
-
-class Team < ActiveRecord::Base
-  belongs_to :group
-  has_one :stat
-  has_and_belongs_to_many :games
-end
-
-class Group < ActiveRecord::Base
-  has_many :teams
-end
-
-class Stat < ActiveRecord::Base
-  belongs_to :team
-end
-
-class Game < ActiveRecord::Base
-  has_and_belongs_to_many :teams
-  has_one :result
-end
-
-class Result < ActiveRecord::Base
-  belongs_to :game
-end
 
 configure do
   set :public_folder, Proc.new { File.join(root, "assets") }
   set :views, Proc.new { File.join(root, "templates") }
   set :partial_template_engine, :erb
-  enable :sessions
 end
 
 get '/' do
@@ -50,28 +27,6 @@ get '/' do
   erb :index
 end
 
-get '/teams' do
-  @teams = Team.all
-  @groups = Group.all
-  @stats = Stat.all
-  @games = Game.all
-  @results = Result.all
-  erb :teams
+after do
+  ActiveRecord::Base.connection.close
 end
-
-post '/teams' do
-  @team = Team.new(params)
-  @team.save
-  redirect to('/teams')
-end
-
-delete '/teams' do
-  @team = Team.find_by(:name => params[:name])
-  @team.destroy
-  redirect to('/teams')
-end
-
-
-
-
-
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20140215111517) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: true do |t|
     t.datetime "date"
     t.string   "place"
